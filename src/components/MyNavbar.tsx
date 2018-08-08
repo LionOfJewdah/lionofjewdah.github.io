e877e17d5995a10143014d9d6999006cc3443262
David Silverstone <dsilvers@usc.edu>
2018-08-08 13:24:55 -0700

    Revert resume link change.

--- src/components/MyNavbar.tsx
+++ src/components/MyNavbar.tsx
@@ -5,16 +5,6 @@ import { MenuItem, Nav, Navbar, NavDropdown, NavItem } from "react-bootstrap";
 const resume = "/documents/resume.pdf";
 const linkedinProfile = "https://www.linkedin.com/in/david-silverstone/";
 
-function goToResume(): void {
-	openInNewTab(resume);
-}
-
-function openInNewTab(url: string): void {
-	const win = window.open(url, '_blank');
-	if (win) { win.focus(); }
-}
-
-
 export class MyNavbar extends React.Component {
 	public render() {
 		return <Navbar brand="dsilv.io" inverse={true} fixedTop={true} fluid={true}
@@ -30,10 +20,7 @@ export class MyNavbar extends React.Component {
 					<NavItem eventKey={1} href={linkedinProfile} className="linkedin">
 						LinkedIn
 					</NavItem>
-					<NavItem eventKey={2}
-						onClick={goToResume}
-					// href={resume}
-					>
+					<NavItem eventKey={2} href={resume}>
 						Resume
 					</NavItem>
 					<NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
